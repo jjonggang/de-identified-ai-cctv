2a4495309f081ef0b97eae42883e5b466ec57bd4
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-05-29 23:19:53 +0900

    web socket token 추가

--- src/main/java/com/example/aicctvbackend/socket/WebSocketConfig.java
+++ src/main/java/com/example/aicctvbackend/socket/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/log-alert").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "/cctv/log-alert").setAllowedOrigins("*");
     }
 
 }
